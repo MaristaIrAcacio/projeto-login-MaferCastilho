1f0cb671a6c0e72e966b885cbbbd07e1f2cb9cee
Maria Fernanda Castilho <castilhomariafer13@gmail.com>
2023-07-07 12:13:13 -0300

    descrição em README

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Projeto de construção de login com redirecionamento de página utilizando linguagem javascript, da diciplina de programação web I, do professor Leonardo.
 Nesse projeto entendemos como utilizar javascript para redirecionar páginas html bem como compreendemos a utilização de laços de repetição utilizando for, também com a mesma linguagem.
 
+dia 07/07 
+Adicionamos ao aula2 o input e criamos uma tabela
+e em javaScript criamos a ARRAY
+criamos uma função para atabela
+e criampos um laço de repetição para colocar nome na tabela
--- login.js
+++ login.js
@@ -1,15 +1,36 @@
-/*
-Código de validação de formulário de login
-*/
 function Validar(){
     let email = document.getElementById('email').value;
     let senha = document.getElementById('password').value;
     
-// || !cpf || !rua || !numero || !cep || cidade || !bairro || !estado || !complemento
-
     if(!email || !senha){
         alert("Campos de preenchimento obrigatório. Favor preecher");
     }else{
-        alert("Campos preechidos com sucesso!");
+        window.location.href = "aula2.html";
     }
 }
+    // CRIA LISTA DE USUÁRIO DENTRO DE UM ARRAY
+    var dadosLista = [];
+
+    function salvarUser(){
+        let nomeUser = document.getElementById("nomeUser").value;
+
+        if(nomeUser){
+            dadosLista.push(nomeUser);
+            criaLista();
+            document.getElementById("nomeUser").value = '';
+        }else{
+            alert("Usuário por favor preencher o campo nome");
+        }
+    }
+    // FUNÇÃO PARA CRIAÇÃO DE LISTA NA TABELA
+    function criaLista(){
+        let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+
+        // LAÇO DE REPETIÇÃO PARA COLOCAR NOMES NA TABELA
+        for(let i=0;i <= (dadosLista.length-1);i++){
+            tabela += "<tr><td>"  + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button></td></tr>";
+            document.getElementById("tabela").innerHTML = tabela;
+        }
+    } 
+
+
